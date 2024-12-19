2daa1450d93dda20bef27298dd8915ce5e457133
Nicolas Campos <nicolas.campos611@gmail.com>
2024-12-19 15:47:50 -0300

    Atualizado

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,22 +1,42 @@
 import { Component, signal } from '@angular/core';
 import { FooterComponent } from "./footer/footer.component";
 import { FooterModule } from './footer/footer.module';
+import { CommonModule } from '@angular/common';
+import { HelloWorldService } from '../../services/hello-world.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterModule],
+  imports: [FooterModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
   name = signal("Nicolas");
-  cont = 0;
+  count = 0;
+  value = false;
   
+  lista = []
+
+  constructor(private service: HelloWorldService) {
+    this.service.getHelloWorld().subscribe(
+      {
+        next: (data) => {
+          console.log(data)
+        }, 
+        error: (error) => {
+          console.log(error)
+        }, 
+        complete: () => {
+          console.log("Finalizou")
+        }
+      }
+    )
+  }  
+
 
   teste() {
-    this.cont += 1;
+    this.count += 1;
   }
-
 }
---
+++ src/app/services/hello-world.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelloWorldService {
+
+  private apiUrl = 'https://cat-fact.herokuapp.com/facts';
+
+  constructor(private client: HttpClient) { }
+
+  getHelloWorld(): Observable<any> {
+    return this.client.get(this.apiUrl)
+  }
+}
